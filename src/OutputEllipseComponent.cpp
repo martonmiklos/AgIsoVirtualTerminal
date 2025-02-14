1e6a4134723a6fb807d08e2580e8c8164d201a5f
Miklos Marton <martonmiklosqdev@gmail.com>
2025-02-14 08:05:49 +0100

    Handle drawing of open, closed segment/section ellipses

--- src/OutputEllipseComponent.cpp
+++ src/OutputEllipseComponent.cpp
@@ -15,6 +15,8 @@ OutputEllipseComponent::OutputEllipseComponent(std::shared_ptr<isobus::VirtualTe
 
 void OutputEllipseComponent::paint(Graphics &g)
 {
+	bool fillNeeded = false;
+	isobus::VTColourVector fillColour;
 	// Ensure we fill first, then draw the outline if needed
 	if (isobus::NULL_OBJECT_ID != get_fill_attributes())
 	{
@@ -23,10 +25,8 @@ void OutputEllipseComponent::paint(Graphics &g)
 		if ((nullptr != child) && (isobus::VirtualTerminalObjectType::FillAttributes == child->get_object_type()))
 		{
 			auto fill = std::static_pointer_cast<isobus::FillAttributes>(child);
-
-			auto vtColour = parentWorkingSet->get_colour(fill->get_background_color());
-			g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
-			g.fillEllipse(0, 0, get_width(), get_height());
+			fillColour = parentWorkingSet->get_colour(fill->get_background_color());
+			fillNeeded = true;
 		}
 	}
 
@@ -39,8 +39,90 @@ void OutputEllipseComponent::paint(Graphics &g)
 			auto line = std::static_pointer_cast<isobus::LineAttributes>(child);
 
 			auto vtColour = parentWorkingSet->get_colour(line->get_background_color());
-			g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
-			g.drawEllipse(0, 0, get_width(), get_height(), line->get_width());
+
+			float centerX = get_width() / 2.0f;
+			float centerY = get_height() / 2.0f;
+
+			if (get_ellipse_type() == isobus::OutputEllipse::EllipseType::Closed)
+			{
+				if (get_start_angle() == get_end_angle() && get_ellipse_type() != isobus::OutputEllipse::EllipseType::Closed)
+				{
+					/* If type = closed ellipse segment and start and end angle are the same, a
+					 * single line with width = border width shall be drawn from the centre point to the
+					 * point on the border defined by the start and end angles.*/
+					auto angleRadians = degreesToRadians(-((get_start_angle() * 2.0) - 90));
+					if (angleRadians < 0)
+						angleRadians += 2 * M_PI;
+
+					auto pointX = (centerX + (centerX - (line->get_width() / 2.0f)) * std::cos(angleRadians));
+					auto pointY = (centerY - (centerY - (line->get_height() / 2.0f)) * std::sin(angleRadians));
+
+					g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
+					g.drawLine(centerX, centerY, pointX, pointY, line->get_width());
+				}
+				else
+				{
+					if (fillNeeded)
+					{
+						g.setColour(Colour::fromFloatRGBA(fillColour.r, fillColour.g, fillColour.b, 1.0f));
+						g.fillEllipse(0, 0, get_width(), get_height());
+					}
+					/* If type > 0 (!= Closed) and start and end angles are the same, the ellipse is drawn closed. */
+					g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
+					g.drawEllipse(line->get_width() / 2.0f, line->get_width() / 2.0f, get_width() - line->get_width(), get_height() - line->get_width() - 1, line->get_width());
+				}
+			}
+			else
+			{
+				// Juce coordinate system 0° is at the Y axis positive, calculating clockwise
+				// IsoBus coordinate system 0° is at the X axis positive, calculating counter-clockwise
+				float startAngle = juce::degreesToRadians(-((get_start_angle() * 2.0f) - 90));
+				float endAngle = juce::degreesToRadians(-((get_end_angle() * 2.0f) - 90));
+
+				if (endAngle > startAngle)
+					startAngle += 2 * M_PI;
+
+				juce::Path arcPath;
+				if (get_ellipse_type() == isobus::OutputEllipse::EllipseType::ClosedEllipseSegment)
+				{
+					// segment: the ellipse section endpoints connected to the center with two lines
+					arcPath.startNewSubPath(centerX, centerY);
+				}
+
+				arcPath.addArc(line->get_width() / 2.0f, line->get_width() / 2.0f, get_width() - line->get_width() / 2.0f, get_height() - line->get_height() / 2.0f, startAngle, endAngle, get_ellipse_type() != isobus::OutputEllipse::EllipseType::ClosedEllipseSegment);
+
+				if (get_ellipse_type() == isobus::OutputEllipse::EllipseType::ClosedEllipseSegment)
+				{
+					// segment: the ellipse section endpoints connected to the center with two lines
+					arcPath.lineTo(centerX, centerY);
+					if (fillNeeded)
+					{
+						g.setColour(Colour::fromFloatRGBA(fillColour.r, fillColour.g, fillColour.b, 1.0f));
+						g.fillPath(arcPath);
+					}
+
+					g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
+					g.strokePath(arcPath, juce::PathStrokeType(line->get_width()));
+				}
+				else if (get_ellipse_type() == isobus::OutputEllipse::EllipseType::ClosedEllipseSection)
+				{
+					// section: the ellipse section endpoints connected with a straight line
+					arcPath.closeSubPath();
+					if (fillNeeded)
+					{
+						g.setColour(Colour::fromFloatRGBA(fillColour.r, fillColour.g, fillColour.b, 1.0f));
+						g.fillPath(arcPath);
+					}
+
+					g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
+					g.strokePath(arcPath, juce::PathStrokeType(line->get_width()));
+				}
+				else if (get_ellipse_type() == isobus::OutputEllipse::EllipseType::OpenDefinedByStartEndAngles)
+				{
+					g.setColour(Colour::fromFloatRGBA(vtColour.r, vtColour.g, vtColour.b, 1.0f));
+					g.strokePath(arcPath, juce::PathStrokeType(line->get_width()));
+				}
+			}
 		}
 	}
 }
