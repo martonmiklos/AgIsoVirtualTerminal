6e1d80587f0d1b8d63f9fc830f02f87631281590
Miklos Marton <martonmiklosqdev@gmail.com>
2025-10-10 22:18:51 +0200

    Bump stack to test Open-Agriculture/AgIsoStack-plus-plus#616

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,6 +3,6 @@ if(NOT TARGET isobus::isobus)
   FetchContent_Declare(
     CAN_Stack
     GIT_REPOSITORY https://github.com/Open-Agriculture/AgIsoStack-plus-plus.git
-    GIT_TAG 30e51211a760c2df064dc2934a4f180c4cad73cc)
+    GIT_TAG 3499b10bda3d5164315b9ef0b646907269bac902)
   FetchContent_MakeAvailable(CAN_Stack)
 endif()
